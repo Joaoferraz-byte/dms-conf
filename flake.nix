ec9274c8560512b79319c54a4434b7e3153f341f
Joaoferraz-byte <joaoferraz467@gmail.com>
2026-08-26 13:56:00 +0000

    fix: preserve recorder in host bar overlays

--- flake.nix
+++ flake.nix
@@ -34,11 +34,11 @@
           # desktopProfile contract used by the NixOS host modules.
           batteryOnBar = (desktopProfile.monitorProfile or null) == "latitude";
           barEnd = if batteryOnBar
-            then ''end = ["media", "bar", "notifications", "battery", "session"]''
-            else ''end = ["media", "bar", "notifications", "session"]'';
+            then ''end = ["media", "bar", "recorder", "notifications", "battery", "session"]''
+            else ''end = ["media", "bar", "recorder", "notifications", "session"]'';
           rawSettings = builtins.readFile (self + "/config/noctalia/config.toml");
           settings = pkgs.writeText "noctalia-config.toml" (lib.replaceStrings
-            [ "@NOCTALIA_PALETTE_TEMPLATE@" "@NOCTALIA_NVIM_TEMPLATE@" "@NOCTALIA_FIREFOX_TEMPLATE@" "@NOCTALIA_ZEN_TEMPLATE@" "@NOCTALIA_LAMBDA_ICON@" "@NOCTALIA_DISCORD_TEMPLATE@" "@NOCTALIA_TAUON_TEMPLATE@" "@NOCTALIA_HEROIC_TEMPLATE@" "@NOCTALIA_PRISM_TEMPLATE@" "end = [\"media\", \"bar\", \"notifications\", \"session\"]" ]
+            [ "@NOCTALIA_PALETTE_TEMPLATE@" "@NOCTALIA_NVIM_TEMPLATE@" "@NOCTALIA_FIREFOX_TEMPLATE@" "@NOCTALIA_ZEN_TEMPLATE@" "@NOCTALIA_LAMBDA_ICON@" "@NOCTALIA_DISCORD_TEMPLATE@" "@NOCTALIA_TAUON_TEMPLATE@" "@NOCTALIA_HEROIC_TEMPLATE@" "@NOCTALIA_PRISM_TEMPLATE@" "end = [\"media\", \"bar\", \"recorder\", \"notifications\", \"session\"]" ]
             [ "${self}/config/noctalia/templates/livara-palette.json" "${self}/config/noctalia/templates/nvim-base16.lua" "${self}/config/noctalia/templates/firefox.css" "${self}/config/noctalia/templates/zen-userchrome.css" "${self}/assets/lambda-thick.svg" "${communityTemplates}/discord/discord-material.css" "${communityTemplates}/tauon/tauon.txt" "${communityTemplates}/heroiclauncher/heroic.css" "${communityTemplates}/prismlauncher/prismlauncher.json" barEnd ]
             rawSettings);
         in
